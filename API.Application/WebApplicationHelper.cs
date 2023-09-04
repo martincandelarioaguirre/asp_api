2e1b42fbc2b7723f0bf3a1eb333c75abb46bb176
Martin <m.c.a._@hotmail.com>
2023-09-03 21:38:37 -0600

    Change to split creation and configuration of the web app

--- API.Application/WebApplicationHelper.cs
+++ API.Application/WebApplicationHelper.cs
@@ -1,6 +1,19 @@
 ﻿namespace API.Application
 {
-    public class WebApplicationHelper
+    public static class WebApplicationHelper
     {
+        public static WebApplication CreateApiApplication(this WebApplicationBuilder options)
+        {
+            // Add services to the container
+            return options.Build();
+        }
+
+        public static WebApplication ConfigureApiApplication(this WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            app.UseHttpsRedirection();
+            return app;
+        }
+
     }
 }
