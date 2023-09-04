8ebc765ba8aa54b3c019b464483a995b97e3510d
Martin <m.c.a._@hotmail.com>
2023-09-03 22:06:03 -0600

    Configure CORS

--- API.Application/WebApplicationHelper.cs
+++ API.Application/WebApplicationHelper.cs
@@ -4,9 +4,23 @@ namespace API.Application
 {
     public static class WebApplicationHelper
     {
+        private static readonly string _specificOrigins = "_specificOrigins";
         public static WebApplication CreateApiApplication(this WebApplicationBuilder options)
         {
             // Add services to the container
+
+            // Add Cors
+            options.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: _specificOrigins,
+                    policy =>
+                    {
+                        policy
+                        .WithOrigins("*")
+                        .WithMethods("*")
+                        .WithHeaders("*");
+                    });
+            });
             return options.Build();
         }
 
@@ -14,6 +28,8 @@ namespace API.Application
         {
             // Configure the HTTP request pipeline.
             app.UseHttpsRedirection();
+            // We use defined cors
+            app.UseCors(_specificOrigins);
             app.useEndPointRoutes();
             return app;
         }
